90f033740c47246bf3678b03f385ca7fe5e84a38
nico <nicolas.daix-moreux@ens.uvsq.fr>
2024-10-14 19:37:45 +0200

    Added Node struct, tree decision functions
    Added struct Node that will serve to build up the decision tree. Added different mathematical  to get the max, min and mean of a list of parameters(this will be useful for deciding the threshhold).
    Added random sampling function to get random rows that can serve as sample to determine the treshhold (instead of calculating on the whole dataset). Implemented bestThreshHoldColumn that determines the
    best threshhold for a given value list of a specific feature. The decision is made on the minimization of the weighted variance. Created threshhold struct to hold the information outputed by
    bestThreshHold Column.
    
    Next step will be to start testing the functions to see how well they perform (O(n^2) complexity on  bestThreshholdColumn). Combine random sampling to find the best threshhold for each feature
    and then the best feature on which to threshhold.
    Nicolas

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <sstream>
 #include <iostream>
 #include <variant>
+#include <random>
+#include <algorithm>
 
 using namespace std;
 
@@ -127,6 +129,142 @@ void printParamAndResults(vector<string> header,vector<vector<int>> parameters, 
 
 }
 
+/* DECISION TREE LOGIC
+
+
+
+*/
+//tree structure = leaves & nodes
+struct Node {
+    bool isLeaf;
+    float value; //If leaf node, store predicted value (mean of target values)
+    int featureIndex; //Index of feature to split on 
+    float threshold; //Threshold to split on
+    Node* left; //Left child node
+    Node* right; //Right child node
+
+    //Default
+    Node(): isLeaf(true), value(0.0), featureIndex(-1), threshold(0.0), left(nullptr), right(nullptr) {}
+
+};
+
+
+
+/*
+Functions for tree decisions
+*/
+
+//calculate variance of array of ints 
+float calculateVariance(const vector<int>& result_values){
+    if (result_values.empty()) {
+        printf("result_values empty, can't calculate average! returning 0.0");
+        return 0.0;
+    }
+    float mean=0.0;
+    for (float val: result_values) {
+        mean+=val;
+    }
+    mean /= (float)result_values.size();
+
+    float variance =0.0;
+    for (int val: result_values){
+        variance += ((float)val - mean) * ((float)val -mean );
+    }
+    //Could return variance / (result_values.size() -1) for unbiased estimator;
+    return variance / (float)result_values.size();
+
+}
+
+//instead of trying out all values, get the min, max and mean of a feature list and work from there
+int getMaxFeature(vector<vector<int>> values, int feature_index){
+    int max =0;
+    for (int i =0; i<values.size(); ++i){
+        if(values[i][feature_index]>max) max = values[i][feature_index];
+    }
+    return max;
+}
+
+int getMinFeature(vector<vector<int>> values, int feature_index){
+    int min =999999;
+    for (int i =0; i<values.size(); ++i){
+        if(values[i][feature_index]<min) min = values[i][feature_index];
+    }
+    return min;
+}
+
+float getMeanFeature(vector<vector<int>> values, int feature_index){
+    int mean =0;
+    for (int i =0; i<values.size(); ++i){
+        mean += values[i][feature_index];
+    }
+    float average = (float)mean / ((float)values.size()); 
+    return average;
+}
+
+//Implement random sampling: instead of trying out all the different threshholds, sample for example 30 values and try them out as thresholds.
+
+//findBestSplitRandom(vector<vector<int>> values, vector<float> results, int sample_size)
+
+
+/*For specific feature, find the best threshhold
+    
+*/
+
+struct Threshold {
+    int feature_index;
+    int value;
+    float weighted_variance;
+
+};
+
+Threshold bestThresholdColumn(vector<vector<int>> values, vector<float> results, int column_index){
+
+    int best_threshold = 0;
+    float min_weighted_variance = 999999.0;
+    //For every value in of feature[i], use it as a threshold and see best score
+    for (int i =0; i<values.size(); ++i){
+
+        int threshold = values[i][column_index];
+        vector<int> left;
+        vector<int> right;
+
+        for (int j = 0; j<values.size(); ++j){
+            //Strictly inferior, could also be inferior, could replace by float (int+ 0.5) for better splitting (don't know)
+            if (values[j][column_index] < threshold ){
+                left.push_back(values[j][column_index]);
+            } else {
+                right.push_back(values[j][column_index]);
+            }
+
+        }
+        float weighted_variance = (calculateVariance(left)* (float)left.size() + calculateVariance(right) * (float)right.size())/values.size();
+        if (weighted_variance < min_weighted_variance){
+            min_weighted_variance = weighted_variance;
+            best_threshold = threshold;
+        }
+    }
+
+    return Threshold{column_index, best_threshold, min_weighted_variance};
+}
+
+//Draw unique numbers to serve as indexes for rows in random sampling
+vector<int> drawUniqueNumbers(int n, int rows){
+    if (n > rows +1){
+        throw invalid_argument("rows < number of unique numbers!");
+    }
+
+    vector<int> numbers(rows);
+    for (int i =0; i<rows; ++i){
+        numbers[i] = i;
+    }
+    random_device rd;
+    mt19937 g(rd());
+
+    shuffle(numbers.begin(), numbers.end(), g);
+    vector<int> result (numbers.begin(),numbers.begin()+n );
+    return result;
+
+}
 
 
 int main(){
