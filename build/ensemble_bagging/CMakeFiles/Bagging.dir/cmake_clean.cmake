c413dad1c6df84b2a5c6083602b3338dc76021af
nico <nicolas.daix-moreux@ens.uvsq.fr>
2024-11-28 10:00:20 +0100

    Changed methods in compute loss and negativeGradient in loss_function.cpp

---
+++ build/ensemble_bagging/CMakeFiles/Bagging.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Bagging.dir/bagging.cpp.o"
+  "CMakeFiles/Bagging.dir/bagging.cpp.o.d"
+  "libBagging.a"
+  "libBagging.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Bagging.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/ensemble_bagging/CMakeFiles/Bagging.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Bagging.
+# This may be replaced when dependencies are built.
