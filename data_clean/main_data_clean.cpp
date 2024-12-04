e86b2f6996c054ac152c873b300fa48411c381e1
Brendan-FIXOT <fixotb@DESKTOP-2QD6UEV>
2024-12-04 11:01:19 +0100

    Minor fix on data_clean folder + CMakeFiles.txt added to the same folder
    The minor fix to the data_clean folder was mostly about includes path errors, and translations not done.

--- data_clean/main_data_clean.cpp
+++ data_clean/main_data_clean.cpp
@@ -1,34 +1,30 @@
 #include "data_clean.h"
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
-#include <string>
-#include <cmath>
-#include <algorithm>
-#include <numeric>
 
 int main() {
-    std::string inputFilePath = "/home/yifan/桌面/31_10_ppn/decision_tree_ensemble_model/datasets/15k_random.csv"; // Input CSV file path
-    std::string outputFilePath = "/home/yifan/桌面/31_10_ppn/decision_tree_ensemble_model/datasets/cleaned_data.csv"; // Output CSV file path
+    std::string inputFilePath = "/mnt/d/Desktop/decision_tree_ensemble_model/datasets/15k_random.csv"; // Input CSV file path
+    std::string outputFilePath = "/mnt/d/Desktop/decision_tree_ensemble_model/datasets/cleaned_data.csv"; // Output CSV file path
     std::string header;  // To save the header
     int numBins = 50;  // Number of bins for binning
 
 
     std::vector<DataRow> dataset = readCSV(inputFilePath, header);
+    if (dataset.empty()) {
+        std::cerr << "Error: Unable to read data from " << inputFilePath << std::endl;
+        return -1;
+    }
 
     // cleaning by the Z function
     std::vector<DataRow> cleanedData = removeOutliers(dataset);
-    std::cout << "Z 分数清理已完成。" << std::endl;
+    std::cout << "Z-score cleaning completed." << std::endl;
 
     // cleaning bag
     cleanedData = removeOutliersByBinning(cleanedData, numBins);
-    std::cout << "分箱清理已完成。" << std::endl;
+    std::cout << "Binning-based cleaning completed." << std::endl;
 
     // write the data after cleaning into the new csv
     writeCSV(outputFilePath, header, cleanedData);
 
-    std::cout << "清理后的数据已保存至 " << outputFilePath << std::endl;
+    std::cout << "Cleaned data has been saved to " << outputFilePath << std::endl;
 
     return 0;
 }
